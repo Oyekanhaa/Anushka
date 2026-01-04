ef76b6bea10aff178286b4fb12fea8354167ca61
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-01-04 20:44:35 +0530

    Update play.py

--- AloneMusic/plugins/play/play.py
+++ AloneMusic/plugins/play/play.py
@@ -32,7 +32,7 @@ from config import BANNED_USERS, lyrical
 
 EMOJII = [
     "🔥",
-    "💋",
+    "😒",
     "🥺",
     "😒",
     "💖",
@@ -42,7 +42,7 @@ EMOJII = [
     "🧪",
     "🥰",
     "🚩",
-    "🫦",
+    "🫡",
     "💔",
     "🦠",
     "😓",
